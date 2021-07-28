dc26cef7574ff8037985cbae3dd8cec703a0a7c0
astridli-nice <78584145+astridli-nice@users.noreply.github.com>
2021-07-28 16:57:24 +0800

    将canvas无障碍读屏改动为demo

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # AccessibilityDemo
-本仓库存放 无障碍化 demo
-
-- [x] canvas 画布如何进行无障碍化识别 ———— CanvasDemo
-- [x] tabindex 的作用 ———— TabIndexTestDome
-- [ ] aria 的作用 ———— AriaTestDome
+Created with CodeSandbox
--- src/Demo.js
+++ src/Demo.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./CanvasDemo.css";
-export default class CanvasDemo extends React.Component {
+import "./Demo.css";
+export default class Demo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
